3ad9a4bdfa834b49576ae900250eda80ac17bf01
Thiti Dev <thiti.mwk.main@gmail.com>
2021-02-11 20:46:10 +0700

    :label: Clarified the type for constant

--- aung-pao-find-last-missing-piece.ts
+++ aung-pao-find-last-missing-piece.ts
@@ -1,5 +1,5 @@
 import needle from 'needle';
-const NOT_FOUND:string = 'VOUCHER_NOT_FOUND', POSSIBLE_ALPHABETS = [
+const NOT_FOUND:string = 'VOUCHER_NOT_FOUND', POSSIBLE_ALPHABETS: Array<string> = [
     'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z',
     '1','2','3','4','5','6','7','8','9','0',
     'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
