22b433a0bd7bd6191326abccc0bb66c9c1920047
Thiti Dev <thiti.mwk.main@gmail.com>
2021-02-12 23:47:07 +0700

    update: Refactored aung-pao-find-last-missing-piece.ts and exported for usage

--- aung-pao-find-last-missing-piece.ts
+++ aung-pao-find-last-missing-piece.ts
@@ -4,18 +4,26 @@ const NOT_FOUND:string = 'VOUCHER_NOT_FOUND', POSSIBLE_ALPHABETS: Array<string> 
     '1','2','3','4','5','6','7','8','9','0',
     'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'
 ];
+
+const LINK = {
+    CAMPAIGN_PREFIX: 'https://gift.truemoney.com/campaign/?v=',
+    GENERATE_TRIAL_VERIFY_LINK: (midfix:string) => `https://gift.truemoney.com/campaign/vouchers/${midfix}/verify`
+};
+
 /**
  * This process would take {POSSIBLE_ALPHABETS.length} (req)iteration
  */
-(():void => {
-    if(!process.argv[2]) return console.log('[USAGE]: ts-node aung-pao-find-last-missing-piece.ts eCccjeab61o3Nngd1')
-    const TARGET_CODE:string = process.argv[2] // should be the one with missing last alphabet
+const RUN = (TARGET_MISSING_CODE:string):void => {
+    //if(!process.argv[2]) return console.log('[USAGE]: ts-node aung-pao-find-last-missing-piece.ts eCccjeab61o3Nngd1')
+    //const TARGET_CODE:string = process.argv[2] // should be the one with missing last alphabet
     for(let alphabet of POSSIBLE_ALPHABETS){
-        needle.get(`https://gift.truemoney.com/campaign/vouchers/${TARGET_CODE}${alphabet}/verify`, function(error, response) {
+        needle.get(LINK.GENERATE_TRIAL_VERIFY_LINK(TARGET_MISSING_CODE+alphabet), function(error, response) {
             if(response.body.status.code !== NOT_FOUND){
-                return console.log(TARGET_CODE+alphabet) // in ur console -> see magic ^_^
+                return console.log(`actual_code: ${TARGET_MISSING_CODE+alphabet}\nfull_link: ${LINK.CAMPAIGN_PREFIX}${TARGET_MISSING_CODE+alphabet}`) // in ur console -> see magic ^_^
             }
         });
     }
 
-})()
+}
+
+export default RUN
